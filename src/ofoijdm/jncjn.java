20b51fad5fe0be57ea495c3945a927010a8c4645
Amol Kamate <Amol Kamate@192.168.0.10>
2020-12-13 12:08:46 +0530

    hey i made changes as u say...

--- src/ofoijdm/jncjn.java
+++ src/ofoijdm/jncjn.java
@@ -3,6 +3,7 @@ package ofoijdm;
 public class jncjn {
 	public static void main(String[] args) {
 		System.out.println("skcjbzcmc");
+		System.out.println("ak");
 	}
 
 }
--- src/ofoijdm/suresh.java
+++ src/ofoijdm/suresh.java
@@ -3,8 +3,7 @@ package ofoijdm;
 public class suresh {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+System.out.println("abcd");
 	}
 
 }
